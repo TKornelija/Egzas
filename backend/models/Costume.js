10df4ab9e2770b54457f53e708c852f62277b534
TKornelija <kornelija.tamosiunaite@gmail.com>
2025-10-22 21:31:33 +0300

    costume Schema

--- backend/models/Costume.js
+++ backend/models/Costume.js
@@ -1,13 +1,51 @@
 const mongoose = require('mongoose');
 
-const costumeSchema = new mongoose.Schema({
-  name: String,
-  description: String,
-  price: Number,
-  rentalPrice: Number,
-  available: Boolean,
-  imageUrl: String,
-  category: String,
-}, { timestamps: true });
+const costumeSchema = new mongoose.Schema(
+  {
+    id: {
+      type: Number,
+      required: true,
+      unique: true,
+    },
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    rentalPrice: {
+      type: Number,
+      required: true,
+    },
+    quantity: {
+      type: Number,
+      default: 0,
+    },
+    available: {
+      type: Boolean,
+      default: true,
+    },
+    imageUrls: {
+      type: [String],
+      default: [],
+    },
+    category: {
+      type: String,
+      trim: true,
+    },
+    size: {
+      type: [String], 
+      default: [],
+    },
+  },
+  { timestamps: true }
+);
 
 module.exports = mongoose.model('Costume', costumeSchema);
